f81652107c3da643247c59b13565a4b189013c95
Lucía <luciacarrascoribelles@gmail.com>
2021-07-15 11:57:00 +0200

    Modified export method

--- Table1_DemographicDescription.R
+++ Table1_DemographicDescription.R
@@ -1,7 +1,6 @@
-# library(arsenal)
-
 data <- xlsx::read.xlsx("./Table1_Data.xlsx", sheetIndex = 1)
 
+data$ClinicalSpectrum = factor(data$ClinicalSpectrum, levels = c("Negative", "Asymptomatic or Presymptomatic Infection", "Mild moderate Illness", "Severe + Critical Illness"))
 
 library(compareGroups)
 export2xls(createTable(compareGroups(ClinicalSpectrum~Age+Sex+Profession+Education+MaritalStatus+Nationality+NSymptoms+followUp+SinceDiag+PCR, data = data, method = 2), q.type= c(2,3), show.p.overall = F), file = "Table1.xlsx")
@@ -10,4 +9,5 @@ data%>%group_by(ClinicalSpectrum)%>%summarize(Age = paste0("[", min(Age),"-", ma
                                               NSymptoms = paste0("[", min(NSymptoms),"-", max(NSymptoms), "]"),
                                               FollowUp = paste0("[", min(followUp),"-", max(followUp), "]"),
                                               SinceDiag = paste0("[", min(SinceDiag,na.rm=T),"-", max(SinceDiag,na.rm=T), "]"),)%>%
-  xlsx::write.xlsx("Table1.xlsx", sheetName = "MinMax", append = T)
+  as.data.frame()
+  xlsx::write.xlsx("Table1.xlsx", sheetName = "MinMax", append = T, row.names = F)
---
+++ Table2_PositivityOfSerologies.R
@@ -0,0 +1,15 @@
+data <- xlsx::read.xlsx("./Table2_Data.xlsx", sheetIndex = 1)
+
+library(compareGroups)
+
+data$DaysFromDays = factor(data$DaysFromDays, levels = c("0", "15", "30", "60", "90", "180", "270", "360"))
+
+table2A_Data = droplevels(data[complete.cases(data),])
+table2A <- createTable(compareGroups(DaysFromDays~Pattern, data = table2A_Data, max.ylev = 8), show.p.overall = F)
+
+table2B_Data = droplevels(data[!is.na(data$IgG_c) & !is.na(data$IgM_c), ])
+table2B_Data$Pattern = paste0(table2B_Data$IgM_c, table2B_Data$IgG_c)
+table2B <- createTable(compareGroups(DaysFromDays~Pattern, data = table2B_Data, max.ylev = 8), show.p.overall = F)
+
+export2xls(table2A, file = "Table2A.xlsx")
+export2xls(table2B, file = "Table2B.xlsx")
