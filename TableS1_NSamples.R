6cfb10cdd49d117626739d5f4d3a43ef0ad73ac5
Lucía <luciacarrascoribelles@gmail.com>
2021-07-15 12:51:36 +0200

    Modified exporting method

--- TableS1_NSamples.R
+++ TableS1_NSamples.R
@@ -5,4 +5,4 @@ data$DaysFromDays = factor(data$DaysFromDays, levels = c("0", "15", "30", "60", 
 
 data%>%group_by(DaysFromDays)%>%summarize(MeanDays = mean(DaysFromStart), SDDays = sd(DaysFromStart),
                                           NIgM = sum(!is.na(IgM)), NIgG = sum(!is.na(IgG)), NIgG_S = sum(!is.na(IgS)))%>%
-  xlsx::write.xlsx("TableS1.xlsx")
+  xlsx::write.xlsx("TableS1.xlsx", row.names=F)
