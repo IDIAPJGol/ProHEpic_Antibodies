52d2f64228d37c5744b439dceb868fa53681b3db
Lucia <37804669+lucarri@users.noreply.github.com>
2021-12-21 14:13:59 +0100

    Update TableS2_SymptomsDescription.R
    Correct names

--- TableS2_SymptomsDescription.R
+++ TableS2_SymptomsDescription.R
@@ -2,7 +2,7 @@ library(compareGroups)
 data <- xlsx::read.xlsx("./SynData/TableS2_Data.xlsx", sheetIndex = 1)
 
 
-form <- as.formula(paste0("Sex ~", paste0(names(data%>%select(-ClinicalSpectrum,-Sex)), collapse = "+")))
+form <- as.formula(paste0("Gender ~", paste0(names(data%>%select(-ClinicalSpectrum,-Sex)), collapse = "+")))
 
 tabMild <- createTable(compareGroups(form, data = data%>%filter(stringr::str_detect(ClinicalSpectrum, "Mild")), method = 3), hide.no = 0)
 tabSevere <- createTable(compareGroups(form, data = data%>%filter(stringr::str_detect(ClinicalSpectrum, "Severe")), method = 3), hide.no = 0)
