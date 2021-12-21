10de7a138b131d6e729a9d309114429d8e936e6c
Lucia <37804669+lucarri@users.noreply.github.com>
2021-12-21 14:13:05 +0100

    Merge output tables

--- Table1_DemographicDescription.R
+++ Table1_DemographicDescription.R
@@ -1,7 +1,5 @@
 data <- xlsx::read.xlsx("./SynData/Table1_Data.xlsx", sheetIndex = 1)
 
-data$ClinicalSpectrum = factor(data$ClinicalSpectrum, levels = c("Negative", "Asymptomatic or Presymptomatic Infection", "Mild moderate Illness", "Severe + Critical Illness"))
-
 library(compareGroups)
 export2xls(createTable(compareGroups(ClinicalSpectrum~Age+Sex+Profession+Education+MaritalStatus+Nationality+NSymptoms+followUp+SinceDiag+PCR, data = data, method = 2), q.type= c(2,3), show.p.overall = F), file = "Table1.xlsx")
 export2xls(createTable(compareGroups(~Age+Sex+Profession+Education+MaritalStatus+Nationality+NSymptoms+followUp+SinceDiag+PCR, data = data, method = 2), q.type= c(2,3), show.p.overall = F), file = "Table1_Total.xlsx")
@@ -10,12 +8,12 @@ data%>%group_by(ClinicalSpectrum)%>%summarize(Age = paste0("[", min(Age),"-", ma
                                               NSymptoms = paste0("[", min(NSymptoms,na.rm=T),"-", max(NSymptoms,na.rm=T), "]"),
                                               FollowUp = paste0("[", min(followUp),"-", max(followUp), "]"),
                                               SinceDiag = paste0("[", min(SinceDiag,na.rm=T),"-", max(SinceDiag,na.rm=T), "]"),)%>%
-  as.data.frame()
+  as.data.frame()%>%
   xlsx::write.xlsx("Table1.xlsx", sheetName = "MinMax", append = T, row.names = F)
 
 data%>%summarize(Age = paste0("[", min(Age),"-", max(Age), "]"),
                                               NSymptoms = paste0("[", min(NSymptoms,na.rm=T),"-", max(NSymptoms,na.rm=T), "]"),
                                               FollowUp = paste0("[", min(followUp),"-", max(followUp), "]"),
                                               SinceDiag = paste0("[", min(SinceDiag,na.rm=T),"-", max(SinceDiag,na.rm=T), "]"),)%>%
-  as.data.frame()
-xlsx::write.xlsx("Table1_Overall.xlsx", sheetName = "MinMax", append = T, row.names = F)
+  as.data.frame()%>%
+xlsx::write.xlsx("Table1_Total.xlsx", sheetName = "MinMax", append = T, row.names = F)
